e110b57ab6b6f8a299d9d1f8b6352cdce01156b7
Huy Vo <kingsleyvo1504@gmail.com>
2025-10-14 11:46:15 +0700

    TestGit1

---
+++ TestGit/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace TestGit;
+
+public class Class1
+{
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public static void Main(String[] args)
+    {
+        Console.WriteLine("Hello World!");
+        int a = 5;
+        int b = 10;
+        Console.WriteLine("Sum: " + Add(a, b));
+    }
+
+}
